ee2db14c3157ab776c1903e58525ed43452bca01
WillStephensonxyz <willstephenson66@gmail.com>
2024-07-29 21:34:42 +1000

    added get user agent function

--- isearch.py
+++ isearch.py
@@ -1,19 +1,40 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver 
 import webbrowser 
 import requests 
 import sys 
 # import urllib 
 import json 
 
+def getUserAgent():
+    user_agent = {"User-Agent": ""}
+    driver = webdriver.Firefox()
+    user_agent_key = driver.execute_script("return navigator.userAgent;")
+    user_agent["User-Agent"] = user_agent_key
+    driver.quit()
+    return user_agent
+
 def cliUrls(query): 
+    header = user_agent
     url = f"https://google.com/search?q='{query}'"
-    response = requests.get(url) 
+    response = requests.get(url, headers=user_agent) 
     soup = BeautifulSoup(response.text, "html.parser") 
 
     title_objects = soup.find_all('h3') 
+    links = soup.find_all('a')
 
-    for info in title_objects: 
-        print(info.getText()) 
+    results = [] 
+    
+    for h3 in title_objects: 
+        parent = h3.parent 
+        if parent.name == 'a': 
+            link = parent['href'] 
+            title = h3.getText() 
+            results.append((title, link)) 
+
+    for index, (title, link) in enumerate(results, start=1):
+        print(f"{title} \n {link}") 
+
 
 # def webBrowser(): 
 
@@ -22,4 +43,6 @@ if __name__ == "__main__":
         print("Usage: python3 isearch.py <query>") 
     else: 
         query = ' '.join(sys.argv[1:]) 
+        
+        user_agent = getUserAgent()
         cliUrls(query) 
